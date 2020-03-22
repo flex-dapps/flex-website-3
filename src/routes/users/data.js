19463a706d2db8d403f37791090ac02ff09b6933
Tom Nash <contacttomnash@gmail.com>
2020-03-22 21:58:04 +1100

    Update my description

--- src/routes/users/data.js
+++ src/routes/users/data.js
@@ -17,7 +17,7 @@ export const data = [
       }
     ],
     description:
-      'Described by one witness as "possessing arms and dangerous", the Xcessive Overlord has painted himself as a mysterious and ancient figure, sharing wisdom which he hides within the copious lines of email marketing drivel that are distributed fortnightly. Never trust what you read about him, that is unless the information is read under a full moon.'
+      'Described by one witness as "possessing arms and dangerous", the Xcessive Overlord knows when to cut and run. Sharing battle stories thinly veiled as fortnightly email marketing drivel, the Overlord now works for the mafia as a delivery driver. Information of his underlying motives is sparse, some say he receives infrequent instructions via dead drop.'
   },
   {
     label: "Master Assassin",
