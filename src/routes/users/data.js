200f9834da2ae4f6d36864ee8f38b34a3a33a5c2
Tom Nash <tom@flexdapps.com>
2020-09-11 12:39:44 +1000

    Add Jeanne, redact Mad Desperado [WIP]

--- src/routes/users/data.js
+++ src/routes/users/data.js
@@ -1,222 +1,241 @@
 export const data = [
   {
-    label: "X-cessive Overlord",
-    url: "/img/team/tom.gif",
-    name: "Tom Nash",
+    label: 'X-cessive Overlord',
+    url: '/img/team/tom.gif',
+    name: 'Tom Nash',
     codeName: "['X-cessive Overlord']",
     position: "['CTO', 'Co-Founder']",
     skills: "['Software Architecture', 'Solidity', 'Svelte']",
     special: [
       {
-        key: "Field Status",
-        value: "Legal Alien"
+        key: 'Field Status',
+        value: 'Legal Alien',
       },
       {
-        key: "FAVOURITE STATIONARY",
-        value: "Red String"
-      }
+        key: 'FAVOURITE STATIONARY',
+        value: 'Red String',
+      },
     ],
     description:
-      'Described by one witness as "possessing arms and dangerous", the Xcessive Overlord knows when to cut and run. Sharing battle stories thinly veiled as fortnightly email marketing drivel, the Overlord has earned a reputation as a reliable delivery driver. Information of his underlying motives is sparse, though some say he receives infrequent instructions via dead drop.'
+      'Described by one witness as "possessing arms and dangerous", the Xcessive Overlord knows when to cut and run. Sharing battle stories thinly veiled as fortnightly email marketing drivel, the Overlord has earned a reputation as a reliable delivery driver. Information of his underlying motives is sparse, though some say he receives infrequent instructions via dead drop.',
   },
   {
-    label: "Master Assassin",
-    url: "/img/team/alex.gif",
-    name: "Alexander Ramsey",
+    label: 'Master Assassin',
+    url: '/img/team/alex.gif',
+    name: 'Alexander Ramsey',
     codeName: "['Master Assassin', 'Agyle']",
     position: "['CEO', 'Co-Founder']",
     skills: "['Javascript', 'Visual Design', 'Business']",
     special: [
       {
-        key: "Known Locations",
-        value: "['West Melbourne', 'Durotar', 'Dromund Kaas']"
+        key: 'Known Locations',
+        value: "['West Melbourne', 'Durotar', 'Dromund Kaas']",
       },
       {
-        key: "YEETS PER MINUTE",
-        value: "3.1415926535"
-      }
+        key: 'YEETS PER MINUTE',
+        value: '3.1415926535',
+      },
     ],
     description:
-      "First discovered under a sheet in the back of an '84 Toyota Celica, the Master Assassin is the key that turns all locks. A student of the internet, the Assassin has a view of the world which is both upside and down, as well as back and to the front. You can never be too sure how far away the Master Assassin is from the memetic taps which fill his fuel tanks, but you can be certain that he will find them."
+      "First discovered under a sheet in the back of an '84 Toyota Celica, the Master Assassin is the key that turns all locks. A student of the internet, the Assassin has a view of the world which is both upside and down, as well as back and to the front. You can never be too sure how far away the Master Assassin is from the memetic taps which fill his fuel tanks, but you can be certain that he will find them.",
   },
   {
-    label: "Phantom Professional",
-    url: "/img/team/seb.gif",
-    name: "Sebastian Müller",
-    codeName: "Phantom Professional",
+    label: `Mighty Ambassador`,
+    url: '/img/team/jeanne.gif',
+    name: 'Jeanne Bardin',
+    codeName: `['Mighty Ambassador']`,
+    position: `['COO', 'Deal Master']`,
+    skills: `['Due Diligence', 'Motherhood']`,
+    special: [
+      {
+        key: 'Preferred Governance Model',
+        value: '[REDACTED]',
+      },
+      {
+        key: 'Gender Identity',
+        value: 'Golden Retriever',
+      },
+    ],
+    description: `Forcibly ejected and permanently banned from every communist regime under the sun, the Smilin' Dominator has a habit of embroiling herself in underground commercial enterprise. No unmarked building or shadowy stairwell is safe from the Dominator's sense of what's current. She will find you and she will do business with you.`,
+  },
+  {
+    label: 'Phantom Professional',
+    url: '/img/team/seb.gif',
+    name: 'Sebastian Müller',
+    codeName: `['Phantom Professional']`,
     position: "['Lead Back-end Developer', 'Chief of Linux']",
     skills: "['Solidity', 'Bash', 'Machine Learning']",
     special: [
       {
-        key: "COVID-19 Positive",
-        value: "FALSE"
+        key: 'COVID-19 Positive',
+        value: 'FALSE',
       },
       {
-        key: "Screen Size",
-        value: '2.4"'
-      }
+        key: 'Screen Size',
+        value: '2.4"',
+      },
     ],
     description:
-      "His demeanour always collected, his eyes often heavy, the Phantom Professional has cemented himself as Flex Dapps' command line interaction officer. The Phantom Professional's everyday tasks are highly classified, sometimes even he must grant himself security clearance. Since nobody understands what he does, Flex have made the wise decision not to terminate his employment any time soon."
+      "His demeanour always collected, his eyes often heavy, the Phantom Professional has cemented himself as Flex Dapps' command line interaction officer. The Phantom Professional's everyday tasks are highly classified, sometimes even he must grant himself security clearance. Since nobody understands what he does, Flex have made the wise decision not to terminate his employment any time soon.",
   },
   {
-    label: "Intellectual Genius",
-    url: "/img/team/aaron.gif",
-    name: "[REDACTED]",
-    codeName: "Intellectual Genius",
-    position: "[REDACTED]",
-    skills: "[REDACTED]",
+    label: 'Intellectual Genius',
+    url: '/img/team/aaron.gif',
+    name: '[REDACTED]',
+    codeName: 'Intellectual Genius',
+    position: '[REDACTED]',
+    skills: '[REDACTED]',
     special: [
       {
-        key: "Hair Style",
-        value: "[REDACTED]"
+        key: 'Hair Style',
+        value: '[REDACTED]',
       },
       {
-        key: "Last Successful [REDACTED]",
-        value: "1557294687"
-      }
+        key: 'Last Successful [REDACTED]',
+        value: '1557294687',
+      },
     ],
     description:
-      "One glance at Intellectual Genius will tell you that he means business. That glint in his eye isn't saying \"namaste,\" it's saying [REDACTED]. Intellectual Genius heads up the [REDACTED] campaign for Flex Dapps and has resolved never to rest until [REDACTED]."
+      'One glance at Intellectual Genius will tell you that he means business. That glint in his eye isn\'t saying "namaste," it\'s saying [REDACTED]. Intellectual Genius heads up the [REDACTED] campaign for Flex Dapps and has resolved never to rest until [REDACTED].',
   },
   {
-    label: "Tha Prophet",
-    url: "/img/team/ryan.gif",
-    name: "Ryan Miller",
-    codeName: "Tha Prophet",
+    label: 'Tha Prophet',
+    url: '/img/team/ryan.gif',
+    name: 'Ryan Miller',
+    codeName: `['Tha Prophet']`,
     position: "['Full-stack Developer', 'Dev-Ops']",
     skills: "['Object-Oriented', 'Functional', 'Reactive']",
     special: [
       {
-        key: "Favorite night time snack",
-        value: "Peace and prosperity"
+        key: 'Favorite night time snack',
+        value: 'Peace and prosperity',
       },
       {
-        key: "Spirit Pillar",
-        value: `Roll D6 and multiply by your alacrity rating`
-      }
+        key: 'Spirit Pillar',
+        value: `Roll D6 and multiply by your alacrity rating`,
+      },
     ],
     description:
-      'Tha Prophet travels the land spreading the good word of programming, it is supposed that he channels earthly energies through his special shoes to perform "miracles". Although his true origins are unknown, rumour has it that he healed a .NET developer in Lima and recovered bitcoin private keys from a corrupt drive in Montenegro.'
+      'Tha Prophet travels the land spreading the good word of programming, it is supposed that he channels earthly energies through his special shoes to perform "miracles". Although his true origins are unknown, rumour has it that he healed a .NET developer in Lima and recovered bitcoin private keys from a corrupt drive in Montenegro.',
   },
   {
-    label: "X-pert Mastermind",
-    url: "/img/team/maria.gif",
-    name: "Mariia Tretiakova",
-    codeName: "X-pert Mastermind",
+    label: 'X-pert Mastermind',
+    url: '/img/team/maria.gif',
+    name: 'Mariia Tretiakova',
+    codeName: `['X-pert Mastermind']`,
     position: "['Distributed SysAdmin', 'Operations']",
     skills: "['Infiltration', 'I.T. Systems']",
     special: [
       {
-        key: "Starting Location",
-        value: "Kremlin"
+        key: 'Starting Location',
+        value: 'Kremlin',
       },
       {
-        key: "Win Condition",
-        value: "Delete 4chan"
-      }
+        key: 'Win Condition',
+        value: 'Delete 4chan',
+      },
     ],
-    description: `You don't come to be known as the X-Pert Mastermind by sniffing glue and chasing chickens. While nobody knows the Mastermind's real name (due to it's phonetic complexity), they know her by reputation. She can 3D print foreign passports, turn off websites by frowning at them and blacklist you in over 600 government databases.`
+    description: `You don't come to be known as the X-Pert Mastermind by sniffing glue and chasing chickens. While nobody knows the Mastermind's real name (due to it's phonetic complexity), they know her by reputation. She can 3D print foreign passports, turn off websites by frowning at them and blacklist you in over 600 government databases.`,
   },
   {
-    label: "Fearless Mercenary",
-    url: "/img/team/jonathan.gif",
-    name: "Jonathan Dunne",
+    label: 'Fearless Mercenary',
+    url: '/img/team/jonathan.gif',
+    name: 'Jonathan Dunne',
     codeName: "['Fearless Mercenary, 'Tronathan']",
     position: "['Front-end Developer', '007', 'Delegatee']",
     skills: "['Svelte', 'Meme Hour', 'Presidential Support']",
     special: [
       {
-        key: "Underlying Motives",
-        value: "Unclear"
+        key: 'Underlying Motives',
+        value: 'Unclear',
       },
       {
-        key: "Normie Favour Level",
-        value: `[==========] 20/20`
-      }
+        key: 'Normie Favour Level',
+        value: `[==========] 20/20`,
+      },
     ],
-    description: `Always a student, now a teacher, the Fearless Mercenary lets nothing stand in his way when put to task. Currying favour from those in any position, the Mercenary draws understanding from every aspect of the Normie-ruled universe and knows exactly what it takes to succeed: fresh kicks and flash bling.`
+    description: `Always a student, now a teacher, the Fearless Mercenary lets nothing stand in his way when put to task. Currying favour from those in any position, the Mercenary draws understanding from every aspect of the Normie-ruled universe and knows exactly what it takes to succeed: fresh kicks and flash bling.`,
   },
   {
-    label: "Mad Desperado",
-    url: "/img/team/liam.gif",
-    name: "Liam Aharon",
-    codeName: "Mad Desperado",
-    position: "['Full-stack Developer', 'Stick Artist']",
-    skills: "['NodeJS', 'Cloud Functions']",
+    label: 'Mad Desperado',
+    url: '/img/team/liam.gif',
+    name: '[REDACTED]',
+    codeName: `['Mad Desperado']`,
+    position: '[REDACTED]',
+    skills: '[REDACTED]',
     special: [
       {
-        key: "Neuron Count",
-        value: "Nominal"
+        key: '[REDACTED] Count',
+        value: 'Nominal',
       },
       {
-        key: "Preferred Governance Model",
-        value: `Capitalism`
-      }
+        key: 'Preferred Governance Model',
+        value: '[REDACTED]',
+      },
     ],
-    description: `The journey to Atlantis is one way, which is why the Mad Desperado bought fourteen tickets from an unnamed Dark Web service in 2011. Winner of the inaugural Holochain Meme Hour in his very first week on the job, the Desperado is firmly positioned to reap the long term health benefits of Flex-flavoured fruit.`
+    description: `The journey to [REDACTED] is one way, which is why the Mad Desperado bought fourteen tickets from [REDACTED] in 2011. Winner of the inaugural Holochain Meme Hour in his very first week on the job, the Desperado is firmly positioned to reap the long term health benefits of [REDACTED].`,
   },
   {
-    label: "Midnight Swami",
-    url: "/img/team/dan.gif",
-    name: "Daniel Harrison",
+    label: 'Midnight Swami',
+    url: '/img/team/dan.gif',
+    name: 'Daniel Harrison',
     codeName: "['Midnight Swami', 'The Wizard']",
     position: "['Front-end Developer', 'Floating Mantis']",
     skills: "['React', 'Redux', 'Canvas', 'Web3']",
     special: [
       {
-        key: "FAVOURITE TOOL",
-        value: "Dynamite and Bandaids"
+        key: 'FAVOURITE TOOL',
+        value: 'Dynamite and Bandaids',
       },
       {
-        key: "BLOOD SPILLED",
-        value: "ERR_UINT_OVERFLOW"
-      }
+        key: 'BLOOD SPILLED',
+        value: 'ERR_UINT_OVERFLOW',
+      },
     ],
     description:
-      "Midnight Swami hails from the world's newest nation, the aptly-named New Zealand. His strikingly handsome appearance and charismatic personality gained him a reputation as an extremely nice person. But nice guys get nowhere, so Midnight Swami vowed to live a life committed to writing code behind the veil of multiple 24 inch monitors - flexing his dapps for the world to see."
+      "Midnight Swami hails from the world's newest nation, the aptly-named New Zealand. His strikingly handsome appearance and charismatic personality gained him a reputation as an extremely nice person. But nice guys get nowhere, so Midnight Swami vowed to live a life committed to writing code behind the veil of multiple 24 inch monitors - flexing his dapps for the world to see.",
   },
   {
-    label: "Violent Artist",
-    url: "/img/team/denver.gif",
-    name: "Denver Pallis",
-    codeName: "Violent Artist",
+    label: 'Violent Artist',
+    url: '/img/team/denver.gif',
+    name: 'Denver Pallis',
+    codeName: `['Violent Artist']`,
     position: "['Junior Developer', 'Zoomer']",
     skills: "['Python', 'Memes']",
     special: [
       {
-        key: "GENDER",
-        value: "PAN-ASTRAL FURRYKIN THETA XIN"
+        key: 'GENDER',
+        value: 'PAN-ASTRAL FURRYKIN THETA XIN',
       },
       {
-        key: "PREFERRED LIGHTING",
-        value: "EPILEPSY"
-      }
+        key: 'PREFERRED LIGHTING',
+        value: 'EPILEPSY',
+      },
     ],
     description:
-      "Most hackers sneak in under the cover of night, but not the Violent Artist. Loud, reckless, and almost certainly a hostile threat, 99% of psychologists agree that his state-issued dictionary didn't contain the word 'caution'. It doesn't matter whether it's bits, shells, or villains; bashing is the name of the game for the Violent Artist."
+      "Most hackers sneak in under the cover of night, but not the Violent Artist. Loud, reckless, and almost certainly a hostile threat, 99% of psychologists agree that his state-issued dictionary didn't contain the word 'caution'. It doesn't matter whether it's bits, shells, or villains; bashing is the name of the game for the Violent Artist.",
   },
   {
-    label: "Unknown",
-    url: "/img/team/anonymous.gif",
-    name: "See yourself here?",
-    codeName: "Unknown",
+    label: 'Unknown',
+    url: '/img/team/anonymous.gif',
+    name: 'See yourself here?',
+    codeName: 'Unknown',
     position: "['Whatever you're great at']",
     skills: "['Humane Treatment of Animals', 'Making Ca$h Money']",
     special: [
       {
-        key: "Location",
-        value: "ALL_VALUES"
+        key: 'Location',
+        value: 'ALL_VALUES',
       },
       {
-        key: "Computer Owner",
-        value: "TRUE"
-      }
+        key: 'Computer Owner',
+        value: 'TRUE',
+      },
     ],
     description:
-      "Would you like to work with Flex Dapps? Spin us a fresh diss track and send it to hello@flexdapps.com. We'll get back to you if we think you might be a good fit."
-  }
-];
+      "Would you like to work with Flex Dapps? Spin us a fresh diss track and send it to hello@flexdapps.com. We'll get back to you if we think you might be a good fit.",
+  },
+]
 
 // {
 //   label: 'MidnS',
