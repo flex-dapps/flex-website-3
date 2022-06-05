81cbc50e128744af10f6cff0f0a3781823e064e4
MostlyUnemployed <alexanderjohnramsey@gmail.com>
2022-06-05 15:46:06 +1000

    ✌️

--- src/routes/about/data.js
+++ src/routes/about/data.js
@@ -87,15 +87,15 @@ Thinking about cooking up your own schemes? Need to source talented chefs and ta
 
 const dark = `
 # Gone Dark
-On the 8th of January 2021 shortly after the removal of @realDonaldTrump from twitter, the Flex Dapps website was unavailable for exactly 8 minutes 22 seconds. When it reappeared it was hashed and compared to a prior instance by a community forensic unit who found the hashes to match. Thus it was declared that the site and it’s contents hard not been tampered with.
+On the 8th of January 2021 shortly after the removal of @realDonaldTrump from twitter, the Flex Dapps website was unreachable for exactly 8 minutes 22 seconds. When it reappeared it was hashed and compared to a prior instance by a community forensic unit, the hashes were a match. It was officially declared that the site and its contents hard not been tampered with.
 
 --
 
-However, since this event numerous associates of the community have reported that they remember facts and details about the website and the team to have been different in the years prior. Examples of these include the X-Cessive Overlord having no beard, that the original Flex office had been in a butter factory or that the Violent Artist was actually a woman. The RMIT Blockchain Innovation hub declared in it’s industry analysis that the cause was a crypto-psychological case of the Mandela Effect or [“False Memory”](https://en.wikipedia.org/wiki/False_memory#:~:text=In%202010%2C%20this%20shared%20false,having%20served%20as%20President%20of) likely to occur in DAOs. Wild rumours detailing the gang’s discovery of a multi-verse and subsequent disappearance into other timelines were also been popularised by members of Q-anon and are still discussed on forums like 4chan and the WeWork community message boards.
+Since the site outage numerous associates of the community reported that content on the site did not match what they remembered to be true in the years prior. Examples of these include the X-Cessive Overlord having no beard, that the original Flex office had been in a butter factory or that the Violent Artist was infact the name of a classified operation involving cranes, rather than a person. In an industry analysis performed by the RMIT Blockchain Innovation Hub it was this was named a crypto-psychological case of the Mandela Effect or [“False Memory”](https://en.wikipedia.org/wiki/False_memory#:~:text=In%202010%2C%20this%20shared%20false,having%20served%20as%20President%20of) likely to occur in DAOs. Stories detailing the gang’s discovery of a multi-verse and subsequent disappearance into other timelines were also been popularised by members of Q-anon on forums such as 4chan /biz/ and the WeWork community message boards.
 
 --
 
-In the years that followed, Blockchain Australia commissioned a working group to explore the matter in hopes of uncovering the whereabouts of the country’s once thriving, underground industry.
+In late 2021 Blockchain Australia commissioned a working group to invetigate whereabouts of the Australias infamous team of web3 avengers. In the early weeks of January 2022 when the working group were officially scheduled to report their findings, the CEO of Blockchain Australia resigned. To this day the investigation's findings remain classified.
 `
 
 const copy = [
